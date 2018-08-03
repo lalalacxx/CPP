708763a02d758a335b2abe9a20fadcb6515d9fa8
lalalacxx <2420059729@qq.com>
2018-08-03 03:14:26 -0700

    日期类的实现

--- Date/Date.cpp
+++ Date/Date.cpp
@@ -1,29 +1,55 @@
 #include <iostream>
+#include<assert.h>
+
 using namespace std;
 
 class Date
 {
 public:
-    //判断日期是否合法
-    bool IsInvalid(int year,int month,int day)
+    //判断是否为闰年
+    bool IsLeap(int year)
     {
-        if(year >= 1900 && (month >= 1 && month <= 12) && day)
+        if((year%4 == 0 && year%100!=0)
+           || year%400 == 0)
+            return true;
+        else
+            return false;
+    }
+    //获取每个月的天数
+    int GetMonthDay(int year,int month)
+    {
+        int days[13] = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+        if(month == 2 && IsLeap(year))
+        {
+            ++days[2];
+        }
+        return days[month];
+    }
+    //判断日期是否合法
+    bool IsInvalid()
+    {
+        return _year>=1900 
+            && (_month>=1 && _month<=12)
+            && (_day>=1 && _day<=GetMonthDay(_year,_month));
     }
     //构造函数
     Date(int year,int month,int day)
+        :_year(year)
+         ,_month(month)
+         ,_day(day)
     {
-
+        if(!IsInvalid())
+        {
+            assert(false);
+        }
     }
     //拷贝构造函数
     Date(const Date& d)
     {
-
+        
     }
-    //析构函数
-    ~Date()
-    {
-
-    }
+    //析构函数,这里并没有什么好清理的，因此无需自定义一个析构函数
+    //~Date(){};
     //赋值运算符重载
     //
     //
