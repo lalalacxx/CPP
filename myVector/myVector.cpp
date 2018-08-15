81cf7bfe83968bb2964ad902dc82913edae07bef
lalalacxx <2420059729@qq.com>
2018-08-15 04:59:13 -0700

    模拟实现vector

--- myVector/myVector.cpp
+++ myVector/myVector.cpp
@@ -2,6 +2,38 @@
 template<typename T>
 class Vector
 {
+public:
+    //构造函数
+    Vector()
+        :_start(NULL)
+         ,_end(NULL)
+         ,_endofstorage(NULL)
+    {}
+    //拷贝构造函数
+    Vector(const Vector<T>& v)
+    {
+        _start = new T[v.Size()];
+		for (size_t i = 0; i < v.Size(); ++i)
+		{
+			_start[i] = v._start[i]; // operator=
+		}
+
+		_end =  _start + v.Size();
+		_endofstorage = _start + v.Size();
+
+    }
+    //析构函数
+	~Vector()
+	{
+		if (_start)
+		{
+			delete[] _start;
+		}
+
+		_start = _end = _endofstorage = NULL;
+	}
 private:
-    T *data;
+    T *_start;
+    T *_end;
+    T *_endofstorage;
 };
