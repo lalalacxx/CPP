b20c08241f7a799000741d8fa49111f53ddf150e
lalalacxx <2420059729@qq.com>
2018-08-15 05:10:10 -0700

    模拟实现vector

--- myVector/myVector.cpp
+++ myVector/myVector.cpp
@@ -49,6 +49,83 @@ public:
 	{
 		return *(_start);
 	}
+    //尾插
+	void PushBack(const T& x)
+	{
+		//Insert(0, x);
+		if (_end == _endofstorage)
+		{
+			size_t newcapacity = Capacity() == 0 ? 3 : Capacity()*2;
+			Expand(newcapacity);
+		}
+        //扩容完成以后将元素插入
+		*_end = x;
+        //最终将_end的位置更新
+		++_end;
+	}
+
+	void PopBack()
+	{
+		Erase(Size()-1);
+	}
+
+	void Insert(size_t pos, const T& x)
+	{
+		assert(pos <= Size());
+
+		if (_finish == _endofstorage)
+		{
+			size_t newcapacity = Capacity() == 0 ? 3 : Capacity()*2;
+			Expand(newcapacity);
+		}
+
+		T* end = _finish-1;
+		while (end >= _first+pos)
+		{
+			*(end+1) = *end;
+			--end;
+		}
+
+		//*(_first+pos) = x;
+		_first[pos] = x;
+		++_finish;
+	}	
+
+	void Erase(size_t pos)
+	{
+		assert(pos < Size());
+
+		T* start = _first + pos;
+		while (start < _finish-1)
+		{
+			*start = *(start+1);
+			++start;
+		}
+
+		--_finish;
+	}
+
+	size_t Size() const
+	{
+		return _finish - _first;
+	}
+
+	size_t Capacity() const
+	{
+		return _endofstorage - _first;
+	}
+
+	bool Empty()
+	{
+		return _first == _finish;
+	}
+
+	T& operator[](size_t pos)
+	{
+		assert(pos < Size());
+
+		return _first[pos];
+	}
 private:
     T *_start;
     T *_end;
